145d9e98645ff7c4375169e276e5fc948d6d1f37
CodeBySumant <sumantjaiswal49@outlook.in>
2025-05-23 14:12:00 +0530

    backend fixex

--- Udise-bypass -GP/epgp-python.py
+++ Udise-bypass -GP/epgp-python.py
@@ -25,14 +25,14 @@ options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-driver.get("https://sdms.udiseplus.gov.in/p0/v1/login")
+driver.get("https://sdms.udiseplus.gov.in/p0/v1/login?state-id=110")
 driver.maximize_window()
 
 input_element = driver.find_element(By.CLASS_NAME, "form-control")
 input_element.send_keys("BR71392670")
 
 input_element = driver.find_element(By.ID, "password-field")
-input_element.send_keys("oek#77XM")
+input_element.send_keys("ODdgp53#")
 
 time.sleep(10)
 
@@ -46,22 +46,6 @@ time.sleep(25)
 while True:
     time.sleep(2)
 
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-    # aadhar input box html xpath
-    aadhaar_input_xpath = '/html/body/app-root/app-admin-dashboard/div[2]/div[2]/main/div/div/div/app-edit-student-new-ac/div/div/div/div[2]/div/mat-stepper/div/div[2]/div[1]/div/form/div/app-general-info-edit-new-ac/div[1]/div/div/div/div[2]/form/div[3]/div[1]/div/input'
-    try:
-        # locates aadhar input box by its xpath
-        input_element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, aadhaar_input_xpath))
-        )
-        # default aadhar input
-        input_element.send_keys("999999999999")
-
-    except ElementNotInteractableException:
-        print("The Aadhaar input field is disabled. Skipping this step.")
-
-
     # blood group 
     blood_group_dropdown = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, '//select[@formcontrolname="bloodGroup"]'))
@@ -69,19 +53,11 @@ while True:
     blood_group_select = Select(blood_group_dropdown)
     blood_group_select.select_by_value("9")
 
-    #click on update button
+    # Click on update button
     update_button = WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.XPATH, '//span[@class="mat-button-wrapper" and contains(text(), "Update")]'))
+        EC.presence_of_element_located((By.XPATH, "//button[normalize-space(span/text())='Save']"))
     )
     update_button.click()
-
-    # for age confirmation popup
-    # try:
-    #     waitt = WebDriverWait(driver, 20)
-    #     button_element = waitt.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'I agree, Age is correct')]")))
-    #     button_element.click()
-    # except ElementNotInteractableException:
-    #     print("The Aadhaar input field is disabled. Skipping this step.")
 
     time.sleep(1)
     # Wait for the close button to be present in the DOM
@@ -97,19 +73,6 @@ while True:
     )
     next_button.click()
 
-    # time.sleep(2)
-
-    # Wait for the drop-down to be clickable
-#     drop_down_element = WebDriverWait(driver, 10).until(
-#     EC.presence_of_element_located((By.XPATH, '//select[@formcontrolname="academicStream"]'))
-# )
-
-#     # Use Select class to interact with the drop-down
-#     drop_down_select = Select(drop_down_element)
-
-#     # Select by value '2'
-#     drop_down_select.select_by_value("2")
-    
     time.sleep(2)
 
     # date select
@@ -120,6 +83,17 @@ while True:
     driver.execute_script("arguments[0].click();", date_input_element)
     date_input_element.send_keys("04/04/2023", Keys.ENTER)
 
+    # Locate the <select> element
+    select_element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//select[@formcontrolname='languageGroup']"))
+    )
+
+    # Create a Select object
+    dropdown = Select(select_element)
+
+    # Select by value
+    dropdown.select_by_value("1002")
+
     dynamic_xpath_value_2 = "//select[@formcontrolname='academicStream']/option[@value='2']"
 
     button = WebDriverWait(driver, 10).until(
@@ -127,12 +101,6 @@ while True:
     )
     button.click()
     time.sleep(2)
-
-    # Only for GOVERMENT SCHOOL
-    # radio_button = WebDriverWait(driver, 10).until(
-    # EC.presence_of_element_located((By.XPATH, '//input[@type="radio" and @value="2" and @formcontrolname="facProvYN"]'))
-    # )
-    # radio_button.click()
 
     radio_button = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, '//input[@type="radio" and @value="2" and @formcontrolname="centralScholarship"]'))
